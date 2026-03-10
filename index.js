fbf1afe57349f5dbfb11e017bf5ba83c7563a5f6
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:20:38 -0400

    Change equality checks to strict equality operators
    Updated equality checks to use strict equality.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-console.log('1' == 1); //true
-console.log(1 == true); //true
-console.log("false" == false); // "false" is not false
-console.log("false" == true); // "false" is not true either
-
-if("false")
-   console.log("Hello false!") //but "false" is true enough here
+console.log('1' === 1); //false
+console.log(1 === true); //false
+console.log("true" === true); //false
