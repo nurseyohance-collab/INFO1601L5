31d67f29e3de894d539a08dc4eb980a59f3bf838
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:16:28 -0400

    Replace radius and pi with various number types

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let radius = 7;
-const pi = 3.14;
-
-let area = radius * radius * pi;
+let num1 = 0.1341 // float
+let num2 = 2344 // integer
+let num3 = 1.2e+3 // 12000 scientific notation
+let num4 = Infinity // really large number
+let num5 = 0b1000111 // binary number
+let num6 = 0xFFF // hexadecimal number
+let num7 = 9007199254740991n //BigInteger
