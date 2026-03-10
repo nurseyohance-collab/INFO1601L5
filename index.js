a9e63023a9194502969cff9cb414c0f92e7d9e93
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:19:27 -0400

    Refactor index.js to include type checks
    Removed variable declarations and added type checks with console logs.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-let name = "bob";
-let age = 24;
+console.log('1' == 1); //true
+console.log(1 == true); //true
+console.log("false" == false); // "false" is not false
+console.log("false" == true); // "false" is not true either
 
-console.log(typeof(name))//string
-console.log(typeof(age))//number
-
-console.log(`Hello my name is ${name}, I'm ${age} years old`);
-//Hello my name is bob, I'm 24 years old.
-
-console.log(`I was born in ${2020 - age}`)
-//I was born in 1966
+if("false")
+   console.log("Hello false!") //but "false" is true enough here
