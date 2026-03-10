035e98afd51960d62b92f9763c8561d217175fa9
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:18:22 -0400

    Refactor code to use variables for name and age

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let str1 ="Hello World";
-let str2 = 'Hello World';
-let str3 = `
-    hello 
-    world
-`;// template literal (backtick above tab key)
+let name = "bob";
+let age = 24;
 
-console.log(str1, str2, str3);
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
