91d488fc3c1ba7ef5fc061f01811883cf474b31d
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:30:28 -0400

    Add hello function to index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let root = Math.sqrt(9);// 3
-
-let cubed  = Math.pow(2, 3);// 8
-
-let roundDown = Math.floor(12.3453);// 12
-
-let roundUp = Math.ceil(12.3453);// 13
-
-let absolute = Math.abs(-34);// 34
-
-let randNum = Math.random();// random number between 0 and 1
-
-let pi = Math.pi;// pi in radians * 180 to convert to degrees
-
-//generate a random Integer between a supplied range
-function randInt(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function hello() {
+  console.log("hello");
 }
