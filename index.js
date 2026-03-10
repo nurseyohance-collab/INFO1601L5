ffc496c09829dbca59b7d42814861bc5e51f984d
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:28:15 -0400

    Replace date logic with Math functions
    Refactor code to use various Math functions and remove date calculations.

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-let nowTimestamp = Date.now();//store the current Date
+let root = Math.sqrt(9);// 3
 
-console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
+let cubed  = Math.pow(2, 3);// 8
 
-let now = new Date(nowTimestamp);//today's Date object 
+let roundDown = Math.floor(12.3453);// 12
 
-//months go from 0 - 11
-let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
-//printing the date
+let roundUp = Math.ceil(12.3453);// 13
 
-console.log(date.toLocaleDateString("en-US"));//12/17/2019
-//1 is added to month because January is 0
-console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
+let absolute = Math.abs(-34);// 34
 
-//Calculate difference between two dates in milliseconds
-let difference = now - date;
-//convert milliseconds to days
+let randNum = Math.random();// random number between 0 and 1
 
-let daysBetween = difference * 1.15741e-8;
-console.log(daysBetween);//54.6740790437368
+let pi = Math.pi;// pi in radians * 180 to convert to degrees
+
+//generate a random Integer between a supplied range
+function randInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
