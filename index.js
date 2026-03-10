3827864aa7ec6bd28f91c0b61256d39ba159c620
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:24:41 -0400

    Implement FizzBuzz logic in index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let num =0;
-
-while(num < 10){
-  console.log(num);
-  num++;
-}
-
-for(let i=0; i <10; i ++){
-  console.log(i);
+for(let i=0; i< 50; i++){
+  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+    console.log('fizzbuzz');
+  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+    console.log('fizz');
+  }else if(i%5 === 0){ //no? what about just 5 then?
+    console.log('buzz');
+  }else{              //so its not divisible by 3 or 5 then
+    console.log(i);
+  }
 }
