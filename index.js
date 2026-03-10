5e66666c1b7e512297b9c18238c0f88cc314a4b5
nurseyohance-collab <nurseyohance@gmail.com>
2026-03-09 22:26:14 -0400

    Refactor index.js to handle date and timestamp
    Removed fizzbuzz logic and added timestamp and date calculations.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-for(let i=0; i< 50; i++){
-  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
-    console.log('fizzbuzz');
-  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
-    console.log('fizz');
-  }else if(i%5 === 0){ //no? what about just 5 then?
-    console.log('buzz');
-  }else{              //so its not divisible by 3 or 5 then
-    console.log(i);
-  }
-}
+let nowTimestamp = Date.now();//store the current Date
+
+console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
+
+let now = new Date(nowTimestamp);//today's Date object 
+
+//months go from 0 - 11
+let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
+//printing the date
+
+console.log(date.toLocaleDateString("en-US"));//12/17/2019
+//1 is added to month because January is 0
+console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
+
+//Calculate difference between two dates in milliseconds
+let difference = now - date;
+//convert milliseconds to days
+
+let daysBetween = difference * 1.15741e-8;
+console.log(daysBetween);//54.6740790437368
